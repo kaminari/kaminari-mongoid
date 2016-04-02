12cd87f2435bb5ed4b073b3088f9e3bdb2eda6ea
Akira Matsuda <ronnie@dio.jp>
2016-04-02 20:48:45 +0900

    database_cleaner settings from kaminari gem

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,13 @@
+DatabaseCleaner[:mongoid].strategy = :truncation
+
+RSpec.configure do |config|
+  config.before :suite do
+    DatabaseCleaner.clean_with :truncation
+  end
+  config.before :each do
+    DatabaseCleaner.start
+  end
+  config.after :each do
+    DatabaseCleaner.clean
+  end
+end
