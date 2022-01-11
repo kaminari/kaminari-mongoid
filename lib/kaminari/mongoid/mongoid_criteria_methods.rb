11384472ccd8dd432bdfaae15323d231b017f749
rohit krishna <krishnarohitreddy@gmail.com>
2022-01-11 09:49:22 -0600

    Pagination without finding total count

--- lib/kaminari/mongoid/mongoid_criteria_methods.rb
+++ lib/kaminari/mongoid/mongoid_criteria_methods.rb
@@ -31,13 +31,40 @@ module Kaminari
         end
       end
 
+      def without_count
+        extend ::Kaminari::PaginatableWithoutCount
+      end
+
       private
+
       def unpage
         clone.tap do |crit|
           crit.options.delete :limit
           crit.options.delete :skip
         end
       end
+    end
+  end
+end
+
+module Kaminari
+  module PaginatableWithoutCount
+    # Method used to find last page, force setting to false and then use @records.present?
+    # in views to determine if link_to_next_page needs to be displayed.
+    def last_page?
+      false
+    end
+
+    # Method used to check if current page greater than total pages, force setting to false and then use @records.present?
+    # in views to determine if link_to_next_page needs to be displayed.
+    def out_of_range?
+      false
+    end
+
+    # Force to raise an exception if #total_count is called explicitly.
+    def total_count
+      raise "This scope is marked as a non-count paginable scope and can't be used in combination " \
+            "with `#paginate' or `#page_entries_info'. Use #link_to_next_page or #link_to_previous_page instead."
     end
   end
 end
